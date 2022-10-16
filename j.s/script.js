0b8efd7d8e35515e6e3f4aee77fe6e05ca613f01
gogliko1996 <chakiduli@gmail.com>
2022-10-16 22:36:33 +0400

    nev metod

--- j.s/script.js
+++ j.s/script.js
@@ -1,146 +1,91 @@
 let main = document.getElementById("main");
 let div = document.getElementById("post");
 let clearX = document.getElementById("clearx");
-let p_getpost = document.createElement("p");
+let p = document.createElement("p");
+let form = document.getElementById("form");
+let valuetext = document.getElementById("valuetext");
 
-function getserver (){
-let server = new XMLHttpRequest ();
-server.addEventListener ("load", function (){
-  let serverjson = JSON.parse (this.responseText);
+function getserver(url, serverfor) {
+  let server = new XMLHttpRequest();
+  server.open("GET", url);
+  server.send();
+  server.addEventListener("load", function () {
+    let serverjson = JSON.parse(this.responseText);
+    serverfor(serverjson);
+  });
+}
 
+getserver("https://jsonplaceholder.typicode.com/posts/", function (serverjson) {
   serverjson.forEach((element) => {
-    let divbox = document.createElement("div");
-    divbox.setAttribute ("class", "stl");
-    divbox.setAttribute ("data-id",element.id);
-
-    let h2 = document.createElement("h2");
-    h2.innerHTML = element.title;
-
-    let h3 = document.createElement("h3");
-    h3.innerText = element.id;
-
-    let deletePost = document.createElement("button");
-    deletePost.innerHTML = "delete";
-    deletePost.setAttribute("data-id", element.id);
-
-    divbox.innerHTML = `<i class="fa-solid fa-arrow-right" id="${element.id}"></i>`;
-    divbox.appendChild(deletePost);
-    divbox.appendChild(h2);
-    divbox.appendChild(h3);
-    main.appendChild(divbox);
-
-    document.getElementById(element.id).addEventListener("click", function (ivent){
-      div.style.display = "block";
-      if (element.id == ivent.target.id ){
-        let body = serverjson[element.id-1].body;
-        p_getpost.innerText = body;
-        div.appendChild(p_getpost);
-      }
-    })
-
-    deletePost.addEventListener ("click", function (){
-      divbox.remove();
-    })
-
-    clearX.addEventListener("click", function (){
-      div.style.display = "none";
-      p_getpost.remove();
-    })
+    seivserver(element);
   });
 });
-server.open("GET", "https://jsonplaceholder.typicode.com/posts/");
-server.send();
 
+function seivserver(element) {
+  let divbox = document.createElement("div");
+  divbox.setAttribute("class", "stl");
+  divbox.setAttribute("data-id", element.id);
+
+  let h2 = document.createElement("h2");
+  h2.innerHTML = element.title;
+
+  let h3 = document.createElement("h3");
+  h3.innerText = element.id;
+
+  let deletePost = document.createElement("button");
+  deletePost.innerHTML = "delete";
+  deletePost.setAttribute("data-id", element.id);
+
+  divbox.innerHTML = `<i class="fa-solid fa-arrow-right" id="${element.id}"></i>`;
+  divbox.appendChild(deletePost);
+  divbox.appendChild(h2);
+  divbox.appendChild(h3);
+  main.appendChild(divbox);
+
+  document.getElementById(element.id).addEventListener("click", function () {
+      div.style.display = "block";
+      let url = `https://jsonplaceholder.typicode.com/posts/${element.id}`;
+      getserver(url, function (element) {
+        p.innerText = element.body;
+        div.appendChild(p);
+      });
+    });
+
+  deletePost.addEventListener("click", function () {
+    fetch (`https://jsonplaceholder.typicode.com/posts/${element.id}`,{
+      method:"DELETE",
+    })
+    divbox.remove();
+  });
 }
-getserver ()
 
+document.getElementById("clearx").addEventListener("click", function () {
+  div.style.display = "none";
+  p.innerHTML = "";
+});
 
+document.getElementById("add").addEventListener ("click", function (){
+  form.style.display = "block";
+  document.getElementById("add").style.display = "none";
+  document.getElementById("valuetext").value= "";
 
+})
 
-
-// let main = document.getElementById("main");
-// let div = document.getElementById("post");
-// let clearX = document.getElementById("clearx");
-// let p_getpost = document.createElement("p");
-
-// function getserver (){
-// let server = new XMLHttpRequest ();
-// server.addEventListener ("load", function (){
-//   let serverjson = JSON.parse (this.responseText);
-
-//   serverjson.forEach((element) => {
-//     let divbox = document.createElement("div");
-//     divbox.setAttribute ("class", "stl");
-//     divbox.setAttribute ("data-id",element.id);
-
-//     let h2 = document.createElement("h2");
-//     h2.innerHTML = element.title;
-
-//     let h3 = document.createElement("h3");
-//     h3.innerText = element.id;
-
-//     let deletePost = document.createElement("button");
-//     deletePost.innerHTML = "delete";
-//     deletePost.setAttribute("data-id", element.id);
-
-//     divbox.innerHTML = `<i class="fa-solid fa-arrow-right" id="${element.id}"></i>`;
-//     divbox.appendChild(deletePost);
-//     divbox.appendChild(h2);
-//     divbox.appendChild(h3);
-//     main.appendChild(divbox);
-
-//     document.getElementById(element.id).addEventListener("click", function (){
-//       div.style.display = "block";
-//         let body = serverjson[element.id].body;
-//         p_getpost.innerText = body;
-//         div.appendChild(p_getpost);
-//     })
-
-//     deletePost.addEventListener ("click", function (){
-//       divbox.remove();
-//     })
-
-//     clearX.addEventListener("click", function (){
-//       div.style.display = "none";
-//       p_getpost.remove();
-//     })
-//   });
-// });
-// server.open("GET", "https://jsonplaceholder.typicode.com/posts/");
-// server.send();
-
-// }
-// getserver ()
-
-
-
-
-// let main = document.getElementById("main");
-// let div = document.getElementById("post");
-// let clearX = document.getElementById("clearx");
-// let p_getpost = document.createElement("p");
-
-// function getserver() {
-//   fetch("https://jsonplaceholder.typicode.com/posts/", {
-//     method: "GET",
-   
-//   })
-//     .then(function (server) {
-//       console.log(server);
-//       if (server.status !== 200) {
-//         throw server.status;
-//       }
-//       return server.json;
-//     })
-//     .then(function (serverjson) {
-//       console.log(serverjson);
-       
-//     })
-//     .catch(function (error) {
-//       document.createElement("p");
-//       p_getpost.innerText = "error";
-//       p_getpost.style.color = "red";
-//       main.appendChild(p);
-//     });
-// }
-// getserver();
+form.addEventListener ("submit", function (serveradd){
+  serveradd.preventDefault();
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      "title": serveradd.target[0].value
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      seivserver(json)
+      form.style.display = "none";
+      document.getElementById("add").style.display = "block";
+    });
+})
